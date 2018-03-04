c1487106c8f3bbb40151c02ee65deb7f1c2e17e9
Prathyusha Pillari <33558433+pratpill@users.noreply.github.com>
2018-03-04 13:23:42 -0800

    Update comedy.cpp

--- comedy.cpp
+++ comedy.cpp
@@ -4,23 +4,45 @@
 //	File contains: comedy class definitions
 
 #include "comedy.h"
+#include <iostream>
 
 Comedy::Comedy(int stock, std::string director, std::string title, std::string actor, int year)
 {
-
+    this->stock = stock;
+    this->director = director;
+    this->title = title;
+    this->releaseYear = year;
+    this->key = title + std::to_string (year);
 }
 
 void Comedy::print()
 {
-
+    // F, 10, Nora Ephron, You've Got Mail, 1998
+    // print out the movie type, stock, director, title, and
+    // release year
+    std::cout << "F, " << this->stock << ", " << this->director << ", " <<
+    this->title << ", " << this->releaseYear << std::endl;
 }
 
 bool Comedy::operator>(Comedy & cm)
 {
-	return true; //needs to be completed
+    if (this->title > cm.title){
+        return true;
+    }
+    else if(this->title == cm.title){
+        if (this->releaseYear > cm.releaseYear){
+            return true;
+        }
+    }
+    return false;
 }
 
 bool Comedy::operator==(Comedy & cm)
 {
-	return true; //needs to be completed
+    if (this->title == cm.title && this->releaseYear == cm.releaseYear){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
