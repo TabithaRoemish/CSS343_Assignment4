c29d2f6d11ec38c78b021d72cb01a45b85541484
Prathyusha Pillari <33558433+pratpill@users.noreply.github.com>
2018-03-11 00:56:05 -0800

    Update return.h

--- return.h
+++ return.h
@@ -2,22 +2,28 @@
 //	Programmer: Tabitha Roemish & Prathyusha Pillari
 //	Date: February 23, 2018
 //	File contains: return class delcaration
+//      Inherits from the Command class. Using the provided action 
+//      string for the and virtual execute function we Return the 
+//      desired Movie by searching our BST for it, and updating the stock +1.
+
 
 #ifndef RETURN_H
 #define RETURN_H
 #include "command.h"
 
-
 class Return : public Command
 {
 public:
+        // constructor
 	Return(Customer *custPtr, Movie* mvPtr);
-	~Return() {};
-	virtual void print();
-	virtual void execute();
+	~Return() {}; // destructor
+	virtual void print(); // print function for retuning
+	virtual void execute(); // excecute function
 
 private:
+        // pointer to movie
 	Movie * mvPtr;
+	// pointer to customer
 	Customer * custPtr;
 };
 
