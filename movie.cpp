6b61fda83e364d1fec1afc47a1855b80fbe6a1eb
Prathyusha Pillari <33558433+pratpill@users.noreply.github.com>
2018-03-11 01:11:12 -0800

    Update movie.cpp

--- movie.cpp
+++ movie.cpp
@@ -1,3 +1,10 @@
+//	File Name: ass4.cpp
+//	Programmer: Tabitha Roemish, Prathyusha Pillari 
+//	Date: February 23, 2018
+//	File contains: Movie class declaration
+//		movie class is abstract class for movie types (Comedy[F], Drama[D], Classic[C])
+//     The parent class to Comedy, Drama and Classics classes.
+//     This is an abstract class where the child classes must implement the methods within it.
 
 #include "movie.h"
 #include <iostream>
@@ -7,13 +14,15 @@
 #include "drama.h"
 #include "classic.h"
 
-
-
+// create function for the movie class
 Movie* Movie::create(std::string identifier)
 {
+    // movie object to return
     Movie* mov = NULL;
+    // gets the movie type (C, F or D)
     char movieType = identifier[0];
     
+    // switch statement to create the movie object of correct type
     switch (movieType)
     {
         case 'C': {
@@ -24,29 +33,37 @@ Movie* Movie::create(std::string identifier)
             // print out the movie type, stock, director, title, major
             // actor, release month, and release year
            
-            std::string sC = identifier;
+	    // uses string stream to read in from the string
             std::stringstream ss(identifier);
             int stock = 0;
-			std::string junk = "";
-			std::getline(ss, junk, ' ');
-			ss >> stock;
-			std::getline(ss, junk, ' ');
+	    // junk gets rid of the extra space when we 
+	    // parse using a space deliminator
+	    std::string junk = "";
+	    std::getline(ss, junk, ' ');
+	    // reads in the stock number and stores it
+	    ss >> stock;
+	    std::getline(ss, junk, ' ');
+	    // reads in the director and stores it
             std::string director = "";
             std::getline(ss, director, ',');
+	    // reads in the title and stores it
             std::string title = "";
             std::getline(ss, title, ',');
 			std::getline(ss, junk, ' ');
+	    // reads in the actor's full name and stores it
             std::string actorF = "";
 			std::string actorL = "";
 			std::string actorFullName = "";
             std::getline(ss, actorF, ' ');
 			std::getline(ss, actorL, ' ');
 			actorFullName = actorF + " " + actorL;
-            int month = 0;
+            // reads in the month and year and stores it
+	    int month = 0;
             ss >> month;
             int year = 0;
             ss >> year;
             
+            // creates the movie obbject of type classic
             mov = new Classic(stock, director, title, actorFullName, month, year);
             break;
         }
@@ -106,5 +123,5 @@ Movie* Movie::create(std::string identifier)
         default:  // print that movie type does not exist
             return mov;
     }
-    return mov;
+    return mov; // returns the movie pointer
 }
