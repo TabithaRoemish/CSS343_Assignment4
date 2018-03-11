f1b03ccd20ba42fce158bfc7edae44a5b4dd0dec
Prathyusha Pillari <33558433+pratpill@users.noreply.github.com>
2018-03-11 01:00:54 -0800

    Update movie.h

--- movie.h
+++ movie.h
@@ -3,6 +3,9 @@
 //	Date: February 23, 2018
 //	File contains: Movie class declaration
 //		movie class is abstract class for movie types (Comedy[F], Drama[D], Classic[C])
+//     The parent class to Comedy, Drama and Classics classes.
+//     This is an abstract class where the child classes must implement the methods within it. 
+
 
 #ifndef MOVIE_H
 #define MOVIE_H
@@ -12,11 +15,13 @@
 class Movie
 {
 public:
-	Movie() {};
-	virtual ~Movie() {};
-	static Movie * create(std::string identifier); //return nullptr if error in string
+	Movie() {}; // constructor 
+	virtual ~Movie() {}; // destructor
+	//return nullptr if error in string
+	static Movie * create(std::string identifier); 
 
-	virtual void print() = 0;
+	// abstract functions for child classes to fill in
+	virtual void print() = 0; 
 	virtual bool operator>(Movie & mv) = 0;
 	virtual bool operator<(Movie & mv) = 0;
 	virtual bool operator==(Movie & mv) = 0;
