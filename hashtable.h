f0e8cd74266480813e3a1cf9aa57aee4f4c1fff9
Prathyusha Pillari <33558433+pratpill@users.noreply.github.com>
2018-03-11 01:25:51 -0800

    Update hashtable.h

--- hashtable.h
+++ hashtable.h
@@ -2,6 +2,8 @@
 //	Programmer: Tabitha Roemish
 //	Date: March 3, 2018
 //	File contains: hashtable class declaration 
+//      Hash table to the customer information
+//      Has HashElement and HashMap classes
 
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
@@ -11,9 +13,9 @@ class Customer;
 class HashElement
 {
 public:
-
-	int key;
-	Customer * customer;
+	int key; // key to store in the hash table
+	Customer * customer; // pointer to the customer 
+	// constructor to initialize customer pointer and key
 	HashElement(int ky, Customer* cust) {
 		key = ky;
 		customer = cust;
@@ -21,14 +23,15 @@ public:
 
 };
 
+// Hash map class
 class HashMap
 {
 public:
-	HashMap();
-	~HashMap();
+	HashMap(); // constructor
+	~HashMap(); // destructor
 	int HashFunction(int key);
 	void insert(int key, Customer * custPtr);
-	Customer * search(int key);
+	Customer * search(int key); // search function using key
 	void remove(int key);
 	void print();
 	void clear();
