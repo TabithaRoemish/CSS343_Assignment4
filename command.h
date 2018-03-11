61f81574f2ab85d8846f69eebdb399cf01a33de7
Prathyusha Pillari <33558433+pratpill@users.noreply.github.com>
2018-03-11 01:51:30 -0800

    Update command.h

--- command.h
+++ command.h
@@ -2,7 +2,11 @@
 //	Programmer: Tabitha Roemish & Prathyusha Pillari
 //	Date: February 23, 2018
 //	File contains: Command class declaration
-//		command class is abstract class for movie types (Borrow[B], Return[R], Inventory[I], History[H])
+//		command class is abstract class for movie types 
+//              (Borrow[B], Return[R], Inventory[I], History[H])
+//      Parent class to Borrow, Return, Inventory & History classes. 
+//      This is an abstract class where the child classes must implement the methods within it. 
+
 
 #ifndef COMMAND_H
 #define COMMAND_H
@@ -17,13 +21,16 @@ class Customer;
 class Command
 {
 public:
-	friend class Store;
-	virtual ~Command() {};
-	virtual void print() = 0;
+	// frineds the store class to access it
+	friend class Store; 
+	virtual ~Command() {}; // destructor
+	virtual void print() = 0; 
 	virtual void execute() = 0;
+	// command create function
 	static Command * create(std::string identifier);
 
 private:
+	// make function 
 	static Command * make(char actionType, Customer * cust, Movie * mv);
 };
 
