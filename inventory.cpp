bcf0e6efadf66750668d280893766c92f81848c8
Prathyusha Pillari <33558433+pratpill@users.noreply.github.com>
2018-03-11 01:17:58 -0800

    Update inventory.cpp

--- inventory.cpp
+++ inventory.cpp
@@ -2,16 +2,22 @@
 //	Programmer: Tabitha Roemish & Prathyusha Pillari
 //	Date: March 3, 2018
 //	File contains: Inventory class definition
+//      Inherits from the Command class. The Inventory class has 
+//      one method, getInventory, which prints the inventory of the Store.
 
 #include "inventory.h"
 #include <iostream>
-Inventory::Inventory()
-{}
 
+// constructor
+Inventory::Inventory() {}
+
+// print method
 void Inventory::print()
 {
 	std::cout << "Inventory: ";
 }
+
+// execute method that calls the print inventory
 void Inventory::execute()
 {
 	Store::printInventory();
