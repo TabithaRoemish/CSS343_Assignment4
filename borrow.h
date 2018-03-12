3e8ff6853322b5b5b3dfa8d82441dcf6b6133fa9
Prathyusha Pillari <33558433+pratpill@users.noreply.github.com>
2018-03-11 22:22:46 -0700

    Update borrow.h

--- borrow.h
+++ borrow.h
@@ -2,6 +2,10 @@
 //	Programmer: Tabitha Roemish & Prathyusha Pillari
 //	Date: February 23, 2018
 //	File contains: borrow class declaration
+//      Inherits from the Command class. Using the action string and 
+//      virtual execute function we Borrow the desired Movie by searching 
+//      our BST for it, and updating the stock -1. Assumes we have enough stock to Borrow. 
+
 
 #ifndef BORROW_H
 #define BORROW_H
@@ -10,14 +14,15 @@
 class Borrow : public Command
 {
 public:
+        // constructor
 	Borrow(Customer *custPtr, Movie* mvPtr); 
-	~Borrow() {};
+	~Borrow() {}; // destructor
 	virtual void print();
 	virtual void execute();
 
 private:
-	Movie * mvPtr;
-	Customer * custPtr;
+	Movie * mvPtr; // pointer for movie
+	Customer * custPtr; // pointer for customer
 	int tempStock; // to capture stock at time of borrow
 };
 
