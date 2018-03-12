c156c0bbe5d73eb2100893d6914ef4fa56a4a52b
Prathyusha Pillari <33558433+pratpill@users.noreply.github.com>
2018-03-11 22:38:05 -0700

    Update store.h

--- store.h
+++ store.h
@@ -3,7 +3,8 @@
 //	Date: February 23, 2018
 //	File contains: classic class declaration
 //      The Store class also contains a HashTable for all the Store’s Customers.
-//      Manager for all of the Store’s database which includes all the Movies sorted 
+//      Manager for all of the Store’s database which includes 
+//      all the Movies sorted 
 //      by their types, as well as all the Customers of the Store. 
 //      It is assumed that all input is correctly formatted according to the 
 //      homework specification file.
@@ -36,11 +37,13 @@ public:
 private:
 	// map that stores all the three BST's (Classic, Comedy and Drama BST's)
 	static std::map< std::string, std::map<std::string, 
-		BinarySearchTree<Movie*>>> collection; //holds list of Movie Ptrs
+	//holds list of Movie Ptrs
+	BinarySearchTree<Movie*>>> collection; 
 	static HashMap customerList; //Holds list of customers
 	// list of available commands ex)'B', 'R'
 	static std::set<std::string> commandCodes;  
-	static std::set<std::string> mediaCodes; //list of mediacodes ex) D for DVD
+	// list of mediacodes ex) D for DVD
+	static std::set<std::string> mediaCodes; 
 	static std::set<std::string> movieCodes; //list of movieCodes ex) F, D
 	// holds command pointers to release in destructor
 	static std::queue<Command*> commandPtrs; 	
